f0eb33110639f658270effa77eb179a0b1a164e4
Christof Thalhofer <christof@deganius.de>
2021-03-15 18:00:45 +0100

    gambas3-selftest script to run from outside
    [DEVELOPMENT ENVIRONMENT]
    *NEW: small script 'run.sh' to run the gambas3-selftest with and without JIT from outside

---
+++ app/src/gambas3-selftest/run.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+MYDIR=$(pwd)
+
+# run Full without JIT
+gbx3 -j -T "@Full" $MYDIR
+
+if [ $? -ne 0 ] 
+then
+	echo Test without JIT failed
+	return 1
+fi
+
+# run Full with JIT
+GB_NO_JIT=01 GB_JIT_CFLAGS=-O0 gbx3 -T "@Full" $MYDIR
+
+if [ $? -ne 0 ] 
+then
+	echo Test with JIT failed
+	return 1
+fi
+
+
